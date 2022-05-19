0d7a3bc91ef8b4a170f73f701c81548418c90c47
ozzzzz <you@example.com>
2022-05-20 01:05:50 +0300

    visuals add

--- authapp/views.py
+++ authapp/views.py
@@ -1,12 +1,17 @@
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from authapp.forms import HhUserLoginForm, HhUserRegisterForm, HhUserEditForm, HhUserProfileEditForm
+
+from authapp.forms import HhUserLoginForm, HhUserRegisterForm, HhUserEditForm, HhUserProfileEditForm, SkillEditForm, \
+    SkillCreateForm, UserSkillsForm
+from authapp.models import HhUser, Skills
 from authapp.renderers import UserJSONRenderer
 from authapp.serializers import RegistrationSerializer, LoginSerializer
 
@@ -56,6 +61,7 @@ def edit(request):
         if edit_form.is_valid() and edit_profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('auth:edit'))
+
     else:
         edit_form = HhUserEditForm(instance=request.user)
         edit_profile_form = HhUserProfileEditForm(instance=request.user.hhuserprofile)
@@ -64,6 +70,76 @@ def edit(request):
         'edit_profile_form': edit_profile_form,
     }
     return render(request, 'authapp/edit.html', context)
+
+
+@login_required
+def personal_account(request):
+    context = {
+        'title': 'Главная',
+        'account': HhUser.objects.get(pk=request.user.id),
+        # 'skills':
+    }
+    return render(request, 'authapp/personal_account.html', context)
+
+
+@login_required
+def skills_add(request):
+    if request.method == 'POST' and 'add_user_skill' in request.POST:
+        user_skills_form = UserSkillsForm(request.POST, request.FILES, instance=request.user)
+        if user_skills_form.is_valid():
+            user_skills_form.save()
+    else:
+        user_skills_form = UserSkillsForm(instance=request.user)
+    if request.method == 'POST' and 'add_skill' in request.POST:
+        skill_form = SkillCreateForm(request.POST)
+        if skill_form.is_valid():
+            skill_form.save()
+    else:
+        skill_form = SkillCreateForm()
+    context = {
+        'title': 'Главная',
+        'account': HhUser.objects.get(pk=request.user.id),
+        'skill_form': skill_form,
+        'user_skills_form': user_skills_form,
+    }
+    return render(request, 'authapp/skills_add.html', context)
+
+
+class SkillCreateView(CreateView):
+    model = Skills
+    template_name = 'authapp/skills/skill_form.html'
+    success_url = reverse_lazy('authapp:skills_list')
+    form_class = SkillCreateForm
+
+
+class SkillsListView(ListView):
+    model = Skills
+    template_name = 'authapp/skills/skills.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        context_data['object_list'] = Skills.objects.all()
+        return context_data
+
+
+class SkillUpdateView(UpdateView):
+    model = Skills
+    template_name = 'authapp/skills/skill_form.html'
+    success_url = reverse_lazy('authapp:skills_list')
+    form_class = SkillEditForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'категории/редактирование'
+        return context
+
+
+class SkillDeleteView(DeleteView):
+    model = Skills
+    template_name = 'authapp/skills/skill_delete.html'
+
+    def get_success_url(self):
+        return reverse('authapp:skills_list')
 
 
 class RegistrationAPIView(APIView):
@@ -76,21 +152,16 @@ class RegistrationAPIView(APIView):
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class LoginAPIView(APIView):
-    # permission_classes = (AllowAny,)
-    permission_classes =(AllowAny,)
+    permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
     serializer_class = LoginSerializer
 
     def post(self, request):
-        # print(request)
         user = request.data.get('user', {})
         serializer = self.serializer_class(data=user)
         serializer.is_valid(raise_exception=True)
-        # print(serializer.data)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
--- hhservice/urls.py
+++ hhservice/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from mainapp import views as mainapp
 
 urlpatterns = [
@@ -7,3 +9,5 @@ urlpatterns = [
     path('auth/', include('authapp.urls', namespace='auth')),
     path('', mainapp.index, name='index'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
